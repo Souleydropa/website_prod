7ed94b2d489aab00895e5c8ecec9e42617c6cbdb
Souleydropa <souley.dropa.corporate@gmail.com>
2020-11-12 11:20:05 +0100

    udpate db_connect

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,6 @@
-<?php
-
-define('DB_SRV', 'localhost');
-define('DB_PASSWD', "Management_2018");
-define('DB_USER', 'LeblancJuste');
-define('DB_NAME', 'ocd_corpo');
+define('DB_SRV', 'REDACTED');
+define('DB_PASSWD', "REDACTED");
+define('DB_USER', 'REDACTED');
+define('DB_NAME', 'REDACTED');
 
 $connection = mysqli_connect(DB_SRV, DB_USER, DB_PASSWD, DB_NAME);
-
-if($connection == false){
-
-	die("Error: Connection to Database could not be made." . mysqli_connect_error());
-}
-?>
